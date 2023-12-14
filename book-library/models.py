bd902e6b4751c1b1fce84e81bebc9bcbf2f38928
Brian <mulindijrn@gmail.com>
2023-12-15 02:02:45 +0300

    Define 'Book' model for the 'books' table
    Introduce the 'Book' model representing the 'books' table in the database. The model includes columns for 'id' (primary key), 'title,' 'user_id' (foreign key referencing 'users.id'), and 'genre_id' (foreign key referencing 'genres.id'). Bidirectional relationships are established with the 'User' and 'Genre' models using the 'relationship' attribute and 'back_populates.'

--- book-library/models.py
+++ book-library/models.py
@@ -13,3 +13,12 @@ class User(Base):
     # Relationship with the 'Book' model, back_populates establishes bidirectional relationship
     books = relationship('Book', back_populates='user')
 
+# Book model representing the 'books' table
+class Book(Base):
+    __tablename__ = 'books'
+    id = Column(Integer, primary_key=True)
+    title = Column(String, nullable=False)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    user = relationship('User', back_populates='books') # Relationship back to the 'User' model
+    genre_id = Column(Integer, ForeignKey('genres.id')) # Foreign key relationship with the 'Genre' model
+    genre = relationship('Genre', back_populates='books') # Relationship back to the 'Genre' model
