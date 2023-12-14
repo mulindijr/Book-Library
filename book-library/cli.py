85002b485191e102d1a5fae0f1ff75560aa97363
Brian <mulindijrn@gmail.com>
2023-12-15 02:56:22 +0300

    Implement Add Book Functionality
    Introduce the 'add_book' function to prompt the user for book details, validate input, and add a new book to the database. This function ensures that the associated user and genre are added if they do not exist. Committing the changes to the database after adding the book.

--- book-library/cli.py
+++ book-library/cli.py
@@ -4,3 +4,36 @@ from models import User, Book, Genre, engine
 # Create a Session class linked to the engine
 Session = sessionmaker(bind=engine)
 
+def add_book():
+     # Prompt user for book details
+    user = input('Your name: ')
+    title = input('Book title: ')
+    genre = input('Book genre: ')
+
+    # Validate input
+    if not user or not title or not genre:
+        print("Error: Please provide non-empty names for user, title, and genre.")
+        return
+
+    # Create a new session
+    session = Session()
+    # Check if the user exists, if not, add them
+    user_obj = session.query(User).filter_by(name=user).first()
+    if not user_obj:
+        user_obj = User(name=user)
+        session.add(user_obj)
+        session.commit()
+
+    # Check if the genre exists, if not, add it
+    genre_obj = session.query(Genre).filter_by(name=genre).first()
+    if not genre_obj:
+        genre_obj = Genre(name=genre)
+        session.add(genre_obj)
+        session.commit()
+
+    # Create a new book and add it to the session
+    book = Book(title=title, user=user_obj, genre=genre_obj)
+    session.add(book)
+    # Commit the changes to the database
+    session.commit()
+    print(f'Book "{title}" added successfully!')
